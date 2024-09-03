6f025123b254abb68ab87560a6cee9919c204832
Pon-Rahul <ponrahul55@gmail.com>
2024-09-03 18:06:54 +0530

    Search prducts

---
+++ src/Product.js
@@ -0,0 +1,17 @@
+import drone1 from "../src/Assets/phantom-drone-camera.jpg"
+
+const items = [
+    { image: drone1, name: "Item 1", price: "$10.00", date: "2024-09-01", percentage: "5%" , id: 0},
+    { image: drone1, name: "Item 2", price: "$15.00", date: "2024-09-02", percentage: "10%", id: 1},
+    { image: drone1, name: "Item 3", price: "$20.00", date: "2024-09-03", percentage: "15%" , id: 2},
+    { image: drone1, name: "Item 4", price: "$25.00", date: "2024-09-04", percentage: "20%" , id: 3},
+    { image: drone1, name: "Item 5", price: "$30.00", date: "2024-09-05", percentage: "25%" , id: 4},
+    { image: drone1, name: "Item 6", price: "$35.00", date: "2024-09-06", percentage: "30%" , id: 5},
+    { image: drone1, name: "Item 7", price: "$40.00", date: "2024-09-07", percentage: "35%" , id: 6},
+    { image: drone1, name: "Item 8", price: "$45.00", date: "2024-09-08", percentage: "40%" , id: 7},
+    { image: drone1, name: "Item 9", price: "$50.00", date: "2024-09-09", percentage: "45%" , id: 8},
+    { image: drone1, name: "Item 10", price: "$55.00", date: "2024-09-10", percentage: "50%" , id: 9}
+  ];
+
+  
+  export default items;
--- src/SearchPage/SearchPage.js
+++ src/SearchPage/SearchPage.js
@@ -1,12 +1,29 @@
-import React from "react";
-import "./SearchPage.css"
+import React, { useState } from "react";
+import "./SearchPage.css";
+import items from "../Product";
 
-const SearchPage = () =>{
-    return(
-        <div>
-        hello
+const SearchPage = () => {
+  const [product, setProduct] = useState(items);
+
+  return (
+    <div>
+      <div>
+      {product.map((o, i) => (
+        <div className="product">
+          <div>
+            <img src={o.image} alt="background" className="thumbnail" />
+          </div>
+          <div className="details">
+            <div>{o.name}</div>
+            <div>{o.price} (12% off)</div>
+            <div>FREE delivery on {o.date}</div>
+            <div><button className="button-32">Add to cart</button></div>
+          </div>
         </div>
-    )
-}
+      ))}
+      </div>
+    </div>
+  );
+};
 
 export default SearchPage;
